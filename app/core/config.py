ceabbeebed9fe1a1fc3b88816db3f49cdb7da181
Bneli_edika <giorgi.kuloshvili@makingscience.com>
2025-12-11 12:32:49 +0400

    config created

---
+++ app/core/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+class Settings(BaseSettings):
+    HTML_STORAGE_PATH: Path = BASE_DIR / "storage" / "html"
+    MARKDOWN_STORAGE_PATH: Path = BASE_DIR / "storage" / "markdown"
